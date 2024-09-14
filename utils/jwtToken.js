9ec6efef3de8c237d0b9d81a831ea4d195044b21
Musa Eleazar <musaeleazar090@gmail.com>
2024-09-14 07:54:46 +0100

    Added token: to jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,14 +4,17 @@ import { generateToken } from "../utils/authHelpers.js";
 const sendToken = async (user, statusCode, res) => {
   //const token = user.getJWTToken();
   const token = await generateToken(user._id);
-  res.status(statusCode).cookie("token", token, { secure: false }).json({
-    success: true,
-    message: "Login successfull",
-    data: {
-      user,
-    },
-    token,
-  });
+  res
+    .status(statusCode)
+    .cookie("token", token, { secure: false })
+    .json({
+      success: true,
+      message: "Login successfull",
+      data: {
+        user,
+      },
+      token: `Bearer ${token}`,
+    });
 };
 
 export { sendToken };
